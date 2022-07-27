26f283ebe2af0d1b05fa7a871ace887630889537
sheikhazhanmohammed <stan0azhan@gmail.com>
2022-07-27 17:52:40 +0530

    changed homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `Computer Vision Blog`,
+    siteTitleAlt: `CV Blog`,
+    siteHeadline: `Computer Vision Blog from @azhanmohammed`,
+    siteUrl: `https://www.linkedin.com/in/azhanmohammed/`,
+    siteDescription: `Simple and intuitive computer vision blog focussing on traditional cv algorithms and deep learning models using libraries like pytorch, tensorflow, ncnn and onnx.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@azhanmohammed`,
   },
   plugins: [
     {
@@ -33,12 +33,12 @@ module.exports = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/azhanmohammed/`,
           },
           {
             name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            url: `https://azhanmohammed.netlify.app/`,
           },
         ],
       },
